fa40fe371dba1b23c3ed8b44db6c35680d6bbe8e
Rick van Hattem <Wolph@wol.ph>
2020-11-24 15:59:03 +0100

    made virtualenv and pyenv detection smarter

--- _tmux/tmux.conf.sh
+++ _tmux/tmux.conf.sh
@@ -2,7 +2,7 @@
 
 verify_tmux_version () {
     tmux_home=~/.tmux
-    tmux_version="$(tmux -V | cut -c 6-)"
+    tmux_version="$(tmux -V | grep -oE '\d+\.\d+')"
 
     if [[ $(echo "$tmux_version >= 2.1" | bc) -eq 1 ]] ; then
         tmux source-file "$tmux_home/tmux_2.1_up.conf"
