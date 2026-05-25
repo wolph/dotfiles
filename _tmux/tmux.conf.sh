3400b8e08d19178bcdfc82bc397c19025c85ad38
Rick van Hattem <wolph@wol.ph>
2026-05-25 03:12:51 +0000

    Add portable tmux and shell defaults

--- _tmux/tmux.conf.sh
+++ _tmux/tmux.conf.sh
@@ -6,13 +6,10 @@ verify_tmux_version () {
 
     if [[ $(echo "$tmux_version >= 2.1" | bc) -eq 1 ]] ; then
         tmux source-file "$tmux_home/tmux_2.1_up.conf"
-        exit
     elif [[ $(echo "$tmux_version >= 1.9" | bc) -eq 1 ]] ; then
         tmux source-file "$tmux_home/tmux_1.9_to_2.1.conf"
-        exit
     else
         tmux source-file "$tmux_home/tmux_1.9_down.conf"
-        exit
     fi
 }
 
